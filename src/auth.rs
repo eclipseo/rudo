c1e73ce496bb22f6e7b7f58e94cc95313e01e7a8
Rémi Lauzier <remilauzier@protonmail.com>
2021-03-22 16:33:24 -0400

    correct a clippy warning

--- src/auth.rs
+++ src/auth.rs
@@ -76,16 +76,16 @@ pub fn auth_pam(
         debug!("Token will be read from file");
         let token = session::read_token_file(token_path.to_str().unwrap());
 
-        if token.is_ok() {
+        if let Ok(token) = token {
             debug!("Token has been read from file");
-            result = match token.unwrap().verify_token(&tty_name, tty_uuid) {
+            result = match token.verify_token(&tty_name, tty_uuid) {
                 Ok(()) => true,
                 Err(err) => {
                     info!("{}", err);
                     false
                 }
-            };
         }
+    }
     } else if token_path.exists() && token_path.is_dir() {
         debug!("token_path is a directory and will be erase");
         fs::remove_dir(token_path)?;
